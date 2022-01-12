898d930317db01e80224ba65213cd96a01dae0fd
Luis Monte <luismonte.l560@gmail.com>
2022-01-12 19:54:04 -0300

    6. Crie a função do botão salvar

--- src/App.js
+++ src/App.js
@@ -12,24 +12,26 @@ class App extends React.Component {
       cardAttr2: '',
       cardAttr3: '',
       cardImage: '',
-      cardRare: '',
+      cardRare: 'Normal',
       cardTrunfo: false,
       isSaveButtonDisabled: true,
+      deckMaster: [],
     };
 
-    this.onInputChange = this.onInputChange.bind(this);
-    this.buttonValidation = this.buttonValidation.bind(this);
+    this.handleOnInputChange = this.handleOnInputChange.bind(this);
+    this.handleButtonValidation = this.handleButtonValidation.bind(this);
+    this.handleOnSaveButtonClick = this.handleOnSaveButtonClick.bind(this);
   }
 
-  onInputChange(event) {
+  handleOnInputChange(event) {
     const { target } = event;
     const { name } = target;
     this.setState({
       [name]: target.type === 'checkbox' ? target.checked : target.value,
-    }, () => this.buttonValidation());
+    }, () => this.handleButtonValidation());
   }
 
-  buttonValidation = () => {
+  handleButtonValidation = () => {
     const minNum = 0;
     const maxNum = 90;
     const maxPoints = 210;
@@ -46,14 +48,14 @@ class App extends React.Component {
     if (cardName !== ''
     && cardDescription !== ''
     && cardAttr1 !== ''
-    && cardAttr1 >= minNum
-    && cardAttr1 <= maxNum
+    && Number(cardAttr1) >= minNum
+    && Number(cardAttr1) <= maxNum
     && cardAttr2 !== ''
-    && cardAttr2 >= minNum
-    && cardAttr2 <= maxNum
+    && Number(cardAttr2) >= minNum
+    && Number(cardAttr2) <= maxNum
     && cardAttr3 !== ''
-    && cardAttr3 >= minNum
-    && cardAttr3 <= maxNum
+    && Number(cardAttr3) >= minNum
+    && Number(cardAttr3) <= maxNum
     && Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3) <= maxPoints
     && cardImage !== ''
     ) {
@@ -61,6 +63,31 @@ class App extends React.Component {
     } else {
       this.setState({ isSaveButtonDisabled: true });
     }
+  }
+
+  handleOnSaveButtonClick(event) {
+    event.preventDefault();
+
+    this.setState((prevState) => ({
+      deckMaster: [...prevState.deckMaster, {
+        cardName: prevState.cardName,
+        cardDescription: prevState.cardDescription,
+        cardAttr1: prevState.cardAttr1,
+        cardAttr2: prevState.cardAttr2,
+        cardAttr3: prevState.cardAttr3,
+        cardImage: prevState.cardImage,
+        cardRare: prevState.cardRare,
+        cardTrunfo: prevState.cardTrunfo,
+      }],
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
+      cardImage: '',
+      cardRare: 'Normal',
+    }));
+    console.log(this.deckMaster);
   }
 
   render() {
@@ -79,7 +106,7 @@ class App extends React.Component {
       <div>
         <h1>Tryunfo</h1>
         <Form
-          onInputChange={ this.onInputChange }
+          onInputChange={ this.handleOnInputChange }
           cardName={ cardName }
           cardDescription={ cardDescription }
           cardAttr1={ cardAttr1 }
@@ -89,6 +116,7 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
           isSaveButtonDisabled={ isSaveButtonDisabled }
+          onSaveButtonClick={ this.handleOnSaveButtonClick }
         />
         <Card
           cardName={ cardName }
